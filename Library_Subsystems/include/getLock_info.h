cba2fd74ef8eb40fff38598649cf4b9afbe71579
Tomas Reiter <tommi.reiter@gmx.de>
2026-03-09 11:41:53 +0100

    Add files via upload

---
+++ Library_Subsystems/include/getLock_info.h
@@ -0,0 +1,13 @@
+#define uint32_T uint32_t
+#define uint16_T uint16_t
+#define uint8_T uint8_t
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void getLock_info(uint16_T input); //Info about Communication Locked Mode
+
+#ifdef __cplusplus
+}
+#endif
